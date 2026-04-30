412d323f9c2997bb55a440f884ca29176450e0d3
AlekseiZavorotnyi <zavorotny2005@gmail.com>
2026-04-30 19:50:49 +0300

    bag fix

--- BudjetBackend/src/main/kotlin/config/Http.kt
+++ BudjetBackend/src/main/kotlin/config/Http.kt
@@ -14,18 +14,6 @@ fun Application.configureHttp() {
         header("X-Engine", "Ktor") // will send this header with each response
     }
 
-    install(CORS) {
-        anyHost()
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Get)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Patch)
-        allowMethod(HttpMethod.Delete)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.Accept)
-    }
-
     install(Compression)
     routing {
         swaggerUI(path = "openapi") {
